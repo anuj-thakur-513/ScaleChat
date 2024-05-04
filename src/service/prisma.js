cbf6e549afe2fa67c1170b4c705d9c0e8f81b64f
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2024-05-04 17:21:15 +0530

    update: prismaClient

--- src/service/prisma.js
+++ src/service/prisma.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient({
-  log: ["query", "error", "warn"],
+  log: ["error", "warn"],
 });
 
 module.exports = prisma;
